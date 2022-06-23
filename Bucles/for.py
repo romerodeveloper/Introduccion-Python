8595af218baa725391d2767ea00f38b564ed6a5b
Nicolas Romero <desarrollador.romero@gmail.com>
2022-06-22 23:22:06 -0500

    Documentación de bucles

--- Bucles/for.py
+++ Bucles/for.py
@@ -1,10 +1,21 @@
-#Bucle for recorre datos de una determinada coleccion
+#Bucle for recorre datos de una determinada coleccion con un rango determinado
+
+#Se define la lista
 data = [2,5,6,7,8,1, "nico deja de pensar en ella"]
+
+#El ciclo for recorrera las iteraciones de la lista una por una, la variable i es el iterador
 for i in data:
     print(f"objeto: {i}")
 
 #Ejercicio: sumatoria de todos los numeros entre 0 y 100
+
+#total es una variable resultante que se ira incrementando
 total = 0
+
+#Se define un rango de valores, como si fuera una lista.
 for i in range(0,101):
+    #El ciclo tomara la variable total con un valor inicial y le sumara el valor de la iteracion correspondiente
     total+=i
+
+#El resultado es un ciclo de sumas constantes hasta finalizar el rango
 print(f"el resultado de la suma es: {total}")
--- Bucles/while.py
+++ Bucles/while.py
@@ -1,9 +1,10 @@
-#formato de codigo que se repetira n veces hasta que se cumpla una determinada condicion
-
 import math
+#Ejercicio: Proponer que se solicite un numero positivo, si no cumple la condicion solicitar el numero hasta que sea positivo
 
+#Solicitud del numero
 numero = int(input("Escriba un numero positivo: "))
 
+#Ciclo con la condicion de un numero menor a cero
 while numero<0:
     print("porfavor ingrese un numero positivo")
     numero = int(input("vuelva a ingresar un numero: "))
